426fa4ff4f3bbd0e3deb06b655a4e68c8d049340
RafaelFing25 <rafaelfinger2502@gmail.com>
2022-09-05 23:29:35 -0300

    Testing

--- components/CardMultiple.tsx
+++ components/CardMultiple.tsx
@@ -6,7 +6,7 @@ import styles from '../styles/components/card.module.css'
 interface InputProps {
   letter: string,
   selected: string,
-  onClick?: (event: React.MouseEvent<HTMLButtonElement, globalThis.MouseEvent>)=>void,
+  onClick?: (event: React.MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => void,
   children: String
 }
 
@@ -16,33 +16,38 @@ const CardMultiple: React.FC = () => {
 
   const [selectedLetter, setSelectedLetter] = useState('')
 
-  function makeSelect(event: React.MouseEvent<HTMLButtonElement, globalThis.MouseEvent>){
+  function makeSelect(event: React.MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) {
     //console.log(event.currentTarget.name)
     setSelectedLetter(event.currentTarget.name)
   }
-  useEffect(()=>{
+  useEffect(() => {
     console.log(selectedLetter)
-  },[selectedLetter])
+  }, [selectedLetter])
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Selecionar:</h1>
+      <div className={styles.top}>
+        <h1 className={styles.title}>Selecionar:</h1>
+      </div>
       <div className={styles.question}>
-        <p>Alguma questão de problema exemplo,
-          em que resposta seria de multipla esclha(alternativas)
-          presentes abaixo:</p>
+        <div className={styles.questionText}>
+          <p>Alguma questão de problema exemplo,
+            em que resposta seria de multipla esclha(alternativas)
+            presentes abaixo:</p>
+
+        </div>
         <div className={styles.alternatives}>
           <Input onClick={makeSelect} selected={selectedLetter} letter='a'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id ante odio. Quisque vel sollicitudin magna. Vestibulum est ligula, facilisis sit amet turpis id, condimentum imperdiet felis. Duis est mauris.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel. 
           </Input>
           <Input onClick={makeSelect} selected={selectedLetter} letter='b'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id ante odio. Quisque vel sollicitudin magna. Vestibulum est ligula, facilisis sit amet turpis id, condimentum imperdiet felis. Duis est mauris.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel. 
           </Input>
           <Input onClick={makeSelect} selected={selectedLetter} letter='c'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id ante odio. Quisque vel sollicitudin magna. Vestibulum est ligula, facilisis sit amet turpis id, condimentum imperdiet felis. Duis est mauris.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel. 
           </Input>
           <Input onClick={makeSelect} selected={selectedLetter} letter='d'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id ante odio. Quisque vel sollicitudin magna. Vestibulum est ligula, facilisis sit amet turpis id, condimentum imperdiet felis. Duis est mauris.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel. 
           </Input>
         </div>
       </div>
@@ -55,26 +60,26 @@ const CardMultiple: React.FC = () => {
 
 
 
-const Input: React.FC<InputProps> = ({ children, letter, selected, onClick}) => {
+const Input: React.FC<InputProps> = ({ children, letter, selected, onClick }) => {
   console.log(selected)
   const [className, setClass] = useState(styles.btn)
   const [isSelected, setSelected] = useState(false)
-  
-  useEffect(()=>{
-    if(selected == letter){
+
+  useEffect(() => {
+    if (selected == letter) {
       setSelected(true)
-    }else{
+    } else {
       setSelected(false)
     }
   })
-  useEffect(()=>{
-    if(isSelected){
+  useEffect(() => {
+    if (isSelected) {
       setClass(styles.btnSelected)
-    }else if(selecteds){
+    } else if (selecteds) {
       setClass(styles.btn)
     }
-  },[isSelected])
-  
+  }, [isSelected])
+
   console.log('atualizei', letter)
   return (
     <div className={styles.alternative}>
