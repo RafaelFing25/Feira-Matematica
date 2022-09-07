e5d0038b07242887b52f9e5c63666d4b4c83eaf3
RafaelFing25 <rafaelfinger2502@gmail.com>
2022-09-07 11:05:48 -0300

    Update game.tsx

--- pages/game.tsx
+++ pages/game.tsx
@@ -20,7 +20,9 @@ const game: React.FC = () => {
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,900;1,900&display=swap" rel="stylesheet" />
       </Head>
 
-      <Game sequence={[1]}/>
+      <Game
+      explainCards={[1]}
+       sequence={[1]}/>
     </div>
   );
 }
