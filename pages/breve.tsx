603100e8f3be211ce781eb400ffa89aed6ac867e
RafaelFing25 <rafaelfinger2502@gmail.com>
2022-10-14 10:18:13 -0300

    responsive

---
+++ pages/breve.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from '../styles/Breve.module.css'
+
+
+export default function breve() {
+  return (
+    <div className={styles.container}>Em Breve implementaremos este modo de jogo!</div>
+  )
+}
+
